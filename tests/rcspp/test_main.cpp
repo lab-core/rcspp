8f771e62b1834eb481a20ede9a64cafd0c1039ef
pamun1 <patrick.munroe@gerad.ca>
2026-05-21 14:30:31 -0400

    Fix IntersectionFeasibilityFunction and add tests

--- tests/rcspp/test_main.cpp
+++ tests/rcspp/test_main.cpp
@@ -1,5 +1,6 @@
 #include "test_main.hpp"
 #include "test_label_buckets.hpp"
+#include "resource/concrete/functions/feasibility/test_intersection_feasibility_function.hpp"
 
 #include <iostream>
 
@@ -28,6 +29,11 @@ int main() {
 
     // Test LabelBuckets (BucketLabelList) operations
     p = all_tests_label_buckets();
+    passed += p.first;
+    total += p.second;
+
+    // Test IntersectionFeasibilityFunction (forbidden / required semantics + preprocess)
+    p = all_tests_intersection_feasibility_function();
     passed += p.first;
     total += p.second;
 
